be5db78686933bba1c562c593cca23504c42b214
holms6303 <emoboy108@mail.ru>
2022-05-22 17:27:34 +0300

    Initial Commit

---
+++ CameraChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraChat
+//
+//  Created by Алексей Заметаев on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
