23dc9fffe55663949f896c42d0e0d509cbb943ab
holms6303 <emoboy108@mail.ru>
2022-05-22 17:30:40 +0300

    Initial commit

--- CameraChat/ViewController.swift
+++ CameraChat/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  CameraChat
-//
-//  Created by Алексей Заметаев on 22.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
